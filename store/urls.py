581c0d30f5173d5ce1513f0310a66988a459a812
Shubham Kumar <mr.dev.shubham@gmail.com>
2024-02-02 13:23:17 +0530

    variation code start

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from . import views
 # App Leveal URL 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('<slug:category_slug>/', views.store, name='product_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_details, name='product_details'),
+    path('category/<slug:category_slug>/', views.store, name='product_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', views.product_details, name='product_details'),
+    path('search/', views.search, name='search'),
 ] 
